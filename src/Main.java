3d917c18a360a532e514d5d9f06ca38c1330f96b
Mohamed5amy <mohamed.samy.k@gmail.com>
2015-11-30 21:10:45 +0200

    Tokenize lexical rule input line on operators or space

--- src/Main.java
+++ src/Main.java
@@ -4,66 +4,99 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.Stack;
-
 
 public class Main {
 	private static HashMap<String, String> symbolTable;
 	private Stack<Double> operands = new Stack<>();
 	private Stack<Character> operators = new Stack<>();
-	private static ArrayList<Character> separators = new ArrayList<Character>(Arrays.asList({' ', '-', '|', '+', '*', '(', ')'})); 
-	
+	private static Character[] seps = { ' ', '-', '|', '+', '*', '(', ')' };
+	private static ArrayList<Character> separators = new ArrayList<Character>(
+			Arrays.asList(seps));
+	private static Set<String> definitions = new HashSet<String>();
+	private static ArrayList<String> lineTokens = new ArrayList<String>();
+
 	public static void main(String[] args) {
 		symbolTable = new HashMap<String, String>();
-		try(BufferedReader br = new BufferedReader(new FileReader("/home/hamid/Desktop/rules.txt"))) {
-		    StringBuilder sb = new StringBuilder();
-		    String line = br.readLine();
-		    
-		    sb.append(line);
-	        sb.append(System.lineSeparator());
-	        line = br.readLine();
+		try (BufferedReader br = new BufferedReader(
+				new FileReader(
+						"C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programming Languages Translation\\Project phase 1\\rules.txt"))) {
+			// StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
 
-	        while (line != null){
-		        if(line.charAt(0) == '{'){
-		        	for(String token: line.split(" ")){
-		        		//NFA concatenation of chars
-		        	}
-		        } else if(line.charAt(0) == '['){
-		        	for(int i = 1; i<line.length()-1; i++){
-		        		char parsedChar = line.charAt(i);
-		        		if(parsedChar != '\\' && parsedChar != ' '){
-		        			symbolTable.put(parsedChar + "", parsedChar + "_PUNCT");
-		        		}
-		        	}
-		        } else { // Expressions OR Definitions
-		        	Boolean isDefinition; 
-					if(line.indexOf('=') < line.indexOf(':') || line.indexOf(':') == -1){ 
-						isDefinition = true;
-		        	} else {
-		        		isDefinition = false;
-		        	}
-					
-					if(isDefinition) {
-						line = line.substring(line.indexOf('='));
-					} else {
-						line = line.substring(line.indexOf(':'));
+			// sb.append(line);
+			// sb.append(System.lineSeparator());
+			// line = br.readLine();
+			while (line != null) {
+
+				if (line.charAt(0) == '{') {
+					for (String token : line.substring(1, line.length() - 1)
+							.trim().split(" ")) {
+						// System.out.println("==="+ token+"===");
+						// NFA concatenation of chars
 					}
-		        	for(int i1 = 0, i2 =1; i2 < line.length(); i1++){
-//			        	if(separators.){
-//			        		
-//			        	}
-			        }
-		        }
-		        sb.append(line);
-		        sb.append(System.lineSeparator());
-		        line = br.readLine();
-		    }
-//	        for(String key: symbolTable.keySet()){
-//	        	System.out.println(key + " => " + symbolTable.get(key));
-//	        }
-		    String everything = sb.toString();
+				} else if (line.charAt(0) == '[') {
+					for (int i = 1; i < line.length() - 1; i++) {
+						char parsedChar = line.charAt(i);
+						if (parsedChar != '\\' && parsedChar != ' ') {
+							symbolTable.put(parsedChar + "", parsedChar
+									+ "_PUNCT");
+						}
+					}
+				} else { // Expressions OR Definitions
+					Boolean isDefinition;
+					if ((line.indexOf('=') != -1 && line.indexOf('=') < line
+							.indexOf(':')) || line.indexOf(':') == -1) {
+						isDefinition = true;
+					} else {
+						isDefinition = false;
+					}
+					if (isDefinition) {
+						definitions.add(line.substring(0, line.indexOf('=')).trim());
+//						System.out.println(line.substring(0, line.indexOf('=')).trim() + "=");
+						line = line.substring(line.indexOf('=') + 1);
+					} else {
+						line = line.substring(line.indexOf(':') + 1);
+					}
+					
+					// Tokenize line, separate on operators or space
+					StringBuilder tempChars = new StringBuilder();
+					for (int i1 = 0, i2 = 1; i1 < line.length(); i1++, i2++) {
+						char currentChar = line.charAt(i1);
+						char nextChar = i2 < line.length() ? line.charAt(i2)
+								: ' ';
+						if (currentChar == ' ') {
+							continue;
+						} else {
+							if (currentChar == '\\' && nextChar != 'L') {
+								continue;
+							} else {
+								tempChars.append(currentChar);
+							}
+							if (separators.contains(nextChar)
+									|| separators.contains(currentChar)) {
+								lineTokens.add(tempChars.toString());
+//								System.out.println(tempChars);
+								tempChars = new StringBuilder();
+							} else {
+
+							}
+						}
+					}
+				}
+				// sb.append(line);
+				// sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			// for(String key: symbolTable.keySet()){
+			// System.out.println(key + " => " + symbolTable.get(key));
+			// }
+			// String everything = sb.toString();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
