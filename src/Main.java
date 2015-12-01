6cc09437f385f30e9845ce5900e8bc2a021791da
Mohamed5amy <mohamed.samy.k@gmail.com>
2015-12-01 19:00:30 +0200

    Changing eclipse line width, sorry :D

--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,7 @@ public class Main {
 	private static Stack<NFA> operands;
 	private static Stack<Character> operators;
 	private static Character[] seps = { ' ', '-', '|', '+', '*', '(', ')' };
-	private static ArrayList<Character> separators = new ArrayList<Character>(
-			Arrays.asList(seps));
+	private static ArrayList<Character> separators = new ArrayList<Character>(Arrays.asList(seps));
 	private static HashMap<String, NFA> definitions = new HashMap<String, NFA>(); // TC
 																					// Double
 																					// =>
@@ -27,16 +26,14 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		symbolTable = new HashMap<String, String>();
-		try (BufferedReader br = new BufferedReader(
-				new FileReader(
-						"C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programming Languages Translation\\Project phase 1\\rules.txt"))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(
+				"C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programming Languages Translation\\Project phase 1\\rules.txt"))) {
 			String line = br.readLine();
 			while (line != null) {
 				// Read line, separate on four cases { , [ , expression,
 				// definition
 				if (line.charAt(0) == '{') {
-					for (String token : line.substring(1, line.length() - 1)
-							.trim().split(" ")) {
+					for (String token : line.substring(1, line.length() - 1).trim().split(" ")) {
 						// System.out.println("==="+ token+"===");
 						// NFA concatenation of chars
 					}
@@ -44,14 +41,12 @@ public class Main {
 					for (int i = 1; i < line.length() - 1; i++) {
 						char parsedChar = line.charAt(i);
 						if (parsedChar != '\\' && parsedChar != ' ') {
-							symbolTable.put(parsedChar + "", parsedChar
-									+ "_PUNCT");
+							symbolTable.put(parsedChar + "", parsedChar + "_PUNCT");
 						}
 					}
 				} else { // Expressions OR Definitions
 					Boolean isDefinition;
-					if ((line.indexOf('=') != -1 && line.indexOf('=') < line
-							.indexOf(':')) || line.indexOf(':') == -1) {
+					if ((line.indexOf('=') != -1 && line.indexOf('=') < line.indexOf(':')) || line.indexOf(':') == -1) {
 						isDefinition = true;
 					} else {
 						isDefinition = false;
@@ -69,15 +64,13 @@ public class Main {
 					StringBuilder tempChars = new StringBuilder();
 					for (int i1 = 0, i2 = 1; i1 < line.length(); i1++, i2++) {
 						char currentChar = line.charAt(i1);
-						char nextChar = i2 < line.length() ? line.charAt(i2)
-								: ' ';
+						char nextChar = i2 < line.length() ? line.charAt(i2) : ' ';
 						if (currentChar == ' ') {
 							continue;
 						} else {
 							tempChars.append(currentChar);
 							char prevChar = i1 > 0 ? line.charAt(i1 - 1) : ' ';
-							if ((separators.contains(nextChar) && currentChar != '\\')
-									|| (separators.contains(currentChar) && prevChar != '\\')) {
+							if ((separators.contains(nextChar) && currentChar != '\\') || (separators.contains(currentChar) && prevChar != '\\')) {
 								lineTokens.add(tempChars.toString());
 								// System.out.println(tempChars);
 								tempChars = new StringBuilder();
@@ -94,25 +87,23 @@ public class Main {
 						token = lineTokens.get(i);
 						if (isOperator(token)) { // operator
 							char operator = token.charAt(0);
-							// TC
 							if (operator == ' ') {
-								throw new Exception(
-										"CANNOT BE A SPACE AS OPERATOR!");
+								throw new Exception("CANNOT BE A SPACE AS OPERATOR!");
 							} else if (operator == '-') {
 								// specially treated operator, no precedence
 								// rules, may be implemented in a neat way later
-								char begin = lineTokens.get(i - 1).charAt(0), end = lineTokens
-										.get(i + 1).charAt(0);
+								char begin = lineTokens.get(i - 1).charAt(0), end = lineTokens.get(i + 1).charAt(0);
 								i++;
 								// operands.pop();
 								char index = (char) (begin + 1);
 								// NFA tempNFA = NFA(begin.toString());
+								NFA tempNFA = new NFA();
 								while (index <= end) {
 									// NFA indexNFA = NFA(index.toString());
 									// tempNFA = NFAor(tempNFA, indexNFA);
 									index = (char) (index + 1);
 								}
-								// operands.push(tempNFA);
+								operands.push(tempNFA);
 							} else {
 								// operators: * + | ( ) .
 								parseOperator(operator);
@@ -122,20 +113,10 @@ public class Main {
 								String nextToken = lineTokens.get(i + 1);
 								// Check for concatenation after
 								// + * )
-								if (operator == '+' || operator == '*'
-										|| operator == ')') {
-									if (!isOperator(nextToken)
-											|| nextToken.equals("(")) {
-										System.out
-												.println("CONCAT case [+*)].operand OR ).(: "
-														+ nextToken);
-										// TC
-										// nextOperandNFA =
-										// getOperandNFA(nextToken)
-										// NFA resultNFA = NFAconcat(operandNFA,
-										// nextOperandNFA);
-										// operands.pop();
-										// operands.push(resultNFA);
+								if (operator == '+' || operator == '*' || operator == ')') {
+									if (!isOperator(nextToken) || nextToken.equals("(")) {
+										System.out.println("CONCAT case [+*)].operand OR ).(: " + nextToken);
+										parseOperator('.');
 									}
 								}
 							}
@@ -152,9 +133,7 @@ public class Main {
 							if (i + 1 < lineTokens.size()) {
 								String nextToken = lineTokens.get(i + 1);
 								if (!isOperator(nextToken)) {
-									System.out
-											.println("CONCAT case operand.operand: "
-													+ nextToken);
+									System.out.println("CONCAT case operand.operand: " + nextToken);
 									// TC
 									// nextOperandNFA = getOperandNFA(nextToken)
 									// NFA resultNFA = NFAconcat(operandNFA,
@@ -162,9 +141,7 @@ public class Main {
 									// operands.pop();
 									// operands.push(resultNFA);
 								} else if (nextToken.equals("(")) {
-									System.out
-											.println("CONCAT case operand.(: "
-													+ nextToken);
+									System.out.println("CONCAT case operand.(: " + nextToken);
 									// TC
 									// parseOperator('.',
 									// operators.push('.');
@@ -235,27 +212,22 @@ public class Main {
 		operatorsPrecedence.put('|', 7);
 
 		int currentPrecedence = operatorsPrecedence.get(currentOperator);
-		int stackPeekPrecedence = operators.empty() ? 11 : operatorsPrecedence
-				.get(stackPeek); // 11 not needed, it would enter in
-									// operators.empty case
+		int stackPeekPrecedence = operators.empty() ? 11 : operatorsPrecedence.get(stackPeek); // 11 not needed, it would enter in
+																								// operators.empty case
 		if (currentOperator == ')') {
 			// pop and execute from stack till '('
 			executeBracket();
-		} else if (operators.empty() || operators.peek() == '('
-				|| currentOperator == '('
-				|| currentPrecedence > stackPeekPrecedence) {
+		} else if (operators.empty() || operators.peek() == '(' || currentOperator == '(' || currentPrecedence > stackPeekPrecedence) {
 			operators.push(currentOperator);
 		} else {
 			stackPeek = operators.pop();
 			while (currentPrecedence <= stackPeekPrecedence && stackPeek != '(') {
 				NFA firstOperandNFA = operands.pop();
 				NFA secondOperandNFA = operands.pop();
-				NFA resultNFA = generateNFA(stackPeek, firstOperandNFA,
-						secondOperandNFA);
+				NFA resultNFA = generateNFA(stackPeek, firstOperandNFA, secondOperandNFA);
 				operands.push(resultNFA);
 				stackPeek = operators.empty() ? ' ' : operators.pop();
-				stackPeekPrecedence = operators.empty() ? 11
-						: operatorsPrecedence.get(stackPeek);
+				stackPeekPrecedence = operators.empty() ? 11 : operatorsPrecedence.get(stackPeek);
 			}
 			operators.push(currentOperator);
 		}
@@ -267,16 +239,14 @@ public class Main {
 
 			NFA firstOperandNFA = operands.pop();
 			NFA secondOperandNFA = operands.pop();
-			NFA resultNFA = generateNFA(poppedOperator, firstOperandNFA,
-					secondOperandNFA);
+			NFA resultNFA = generateNFA(poppedOperator, firstOperandNFA, secondOperandNFA);
 			operands.push(resultNFA);
 
 			poppedOperator = operators.pop();
 		}
 	}
 
-	private static NFA generateNFA(Character operator, NFA operand1,
-			NFA operand2) {
+	private static NFA generateNFA(Character operator, NFA operand1, NFA operand2) {
 		switch (operator) {
 		case '.':
 			// NFAconcat(operands1, operand2);
