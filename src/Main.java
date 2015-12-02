b341a9a5dceb92944551ccd2d325d73d504527cc
Mohamed5amy <mohamed.samy.k@gmail.com>
2015-12-02 20:04:48 +0200

    remove os line

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 
-		System.out.println(System.getProperty("os.name"));
 		String os = System.getProperty("os.name");
 		String filePath = os.startsWith("Windows") ? "C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programming Languages Translation\\Project phase 1\\rules.txt"
 				: "/home/hamid/Desktop/rules.txt";
