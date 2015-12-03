bbf1af76eb7966c6677ba8330dae27ec0ae8b07d
Mohamed5amy <mohamed.samy.k@gmail.com>
2015-12-04 00:04:17 +0200

    Added clone to defintions NFAs

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		String os = System.getProperty("os.name");
-		String filePath = os.startsWith("Windows") ? "C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programmingï¿½Languagesï¿½Translation\\Project phase 1\\Compiler\\rules.txt"
+		String filePath = os.startsWith("Windows") ? "C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programming Languages Translation\\Project phase 1\\Compiler\\rules.txt"
 				: "/home/hamid/Desktop/rules.txt";
 		NFAState rulesNFAInitialState = InfixEvaluator.getRulesNFA(filePath);
 		DFAState DFAInitialState = DFAState.generateDFA(rulesNFAInitialState);
