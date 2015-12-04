56f46000ce80cd205e7765b35997a6adc26cc910
Mohamed5amy <mohamed.samy.k@gmail.com>
2015-12-04 16:22:55 +0200

    Adding test program to repo

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		String os = System.getProperty("os.name");
-		String filePath = os.startsWith("Windows") ? "C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programmingï¿½Languagesï¿½Translation\\Project phase 1\\Compiler\\rules.txt"
+		String filePath = os.startsWith("Windows") ? "C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programming Languages Translation\\Project phase 1\\Compiler\\rules.txt"
 				: "/home/hamid/Desktop/rules.txt";
 		NFAState rulesNFAInitialState = InfixEvaluator.getRulesNFA(filePath);
 		DFAState DFAInitialState = DFAState.generateDFA(rulesNFAInitialState);
 //		DFAInitialState.print();
 		System.out.println("****");
-		DFASimulator dfaSimulator = new DFASimulator("/home/hamid/Desktop/text_program.txt", DFAInitialState);
+		DFASimulator dfaSimulator = new DFASimulator("C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programming Languages Translation\\Project phase 1\\Compiler\\test_program.txt", DFAInitialState);
 		dfaSimulator.simulate();
 		
 		// A+B | C*
