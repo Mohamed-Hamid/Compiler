a1dcd9117a82814d232dda27a57f534f22935a09
Hamid <Hamid.CCP@gmail.com>
2015-12-04 23:35:32 +0200

    Removed extra space

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		String os = System.getProperty("os.name");
-		String filePath = os.startsWith("Windows") ? "C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programming Languages Translation\\Project phase 1\\Compiler\\rules.txt"
+		String filePath = os.startsWith("Windows") ? "C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programmingï¿½Languagesï¿½Translation\\Project phase 1\\Compiler\\rules.txt"
 				: "/home/hamid/Desktop/rules.txt";
 		NFAState rulesNFAInitialState = InfixEvaluator.getRulesNFA(filePath);
 		DFAState DFAInitialState = DFAState.generateDFA(rulesNFAInitialState);
 		System.out.println("\n\t\t** DFA Simulation **");
 		DFAState DFAInitialStateMinimized = DFAState.minimizeDFA(DFAInitialState);
-		DFASimulator dfaSimulator = new DFASimulator("C:\\Users\\electric\\Dropbox\\College\\Term 9\\Programming Languages Translation\\Project phase 1\\Compiler\\test_program.txt", DFAInitialStateMinimized, InfixEvaluator.getTokenNames());
+		DFASimulator dfaSimulator = new DFASimulator("/home/hamid/Desktop/text_program.txt", DFAInitialStateMinimized, InfixEvaluator.getTokenNames());
 		dfaSimulator.simulate();
 		System.out.println("\n\t\t** DFA **");
 		DFAInitialStateMinimized.printTable();
